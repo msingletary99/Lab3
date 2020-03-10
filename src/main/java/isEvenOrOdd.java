071a62ddbdbae43d241cad54ee35c1a0b990b2e8
0781317 <0781317@KELCE-LABN10-P.pittstate.edu>
2020-03-10 09:18:54 -0500

    Worked on isEvenOrOdd and isPrime need to add if they are not valid whole numbers statement need to finish isEvenOrOdd

--- src/main/java/isEvenOrOdd.java
+++ src/main/java/isEvenOrOdd.java
@@ -8,17 +8,30 @@
  *
  * @author 0781317
  */
+import java.util.Scanner;
 public class isEvenOrOdd {
-    
+    System.out.println("Please input a whole number");
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int isEvenOrOdd(int number) {
+        int flag = 0;
+        if (number % 2 == 0) {
+            flag = 1;
+        } else {
+            flag = 0;
+        }
+        return flag;
+    }
+
     //create a method
     //create a loop
     //generate 100 random numbers
     //analyze the numbers in a if else
     //if number%2=0, even totalEven=totalEven+1
-        //else, odd, totalOdd=totalOdd+1
-    
+    //else, odd, totalOdd=totalOdd+1
     //display number of even
     //display number of odds
-    
-    
 }
--- src/main/java/isPrime.java
+++ src/main/java/isPrime.java
@@ -8,21 +8,52 @@
  *
  * @author 0781317
  */
+import java.util.Scanner;
+
 public class isPrime {
-    
-    //prompt user to input the whole number they wish to analyze
-    //have user input number and set it equal to some int variable
-    //isPrime(number) --> to method
-    
-//set a method for isPrime procedure
-    //if it is 0 or 1, not prime
-    //else run a do while loop
-    //i=2;i<number; i++
-    //modular division
+
+    public static void main(String[] args) {
+        //prompt user to input the whole number they wish to analyze
+        System.out.println("Plese input a whole number.");
+
+        Scanner keyboard = new Scanner(System.in);
+        int number = keyboard.nextInt();
+
+
+        int flag = isPrime(number);
+
+        if (flag >= 1) {
+            System.out.println("not prime");
+        } else if (flag == 0) {
+            System.out.println("prime");
+        }
+        //have user input number and set it equal to some int variable
+        //isPrime(number) --> to method
+        //set a method for isPrime procedure
+        //if it is 0 or 1, not prime
+        //else run a do while loop
+        //i=2;i<number; i++
+        //modular division
         //create a flag so that it will go until flag is activated
-    //modualr division=1, flag=0
-    //modular division=0, flag=1
-    
-    //when flag is activated number isPrime
-    
+        //modualr division=1, flag=0
+        //modular division=0, flag=1
+        //when flag is activated number isPrime
+    }
+
+    public static int isPrime(int number) {
+        int flag = 0;
+        if (number == 0 || number == 1||number ==2) {
+            flag = 0;
+        } else if (number > 2) {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    flag = flag + 1;
+                }else if(number%i==1){
+                    flag=flag+0;
+                }
+            }
+        }
+
+        return flag;
+    }
 }
